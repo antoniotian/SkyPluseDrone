66db160fac90ce22a2346745a44edcf843637637
antoniotian <antonio.binhan@outlook.com>
2024-05-07 16:06:20 +0100

    Create i2cdriver for Raspberry Pi i2c communication function and try to use Doxygen comment.

---
+++ SkyPulseDrone_FlightControlSystem/DRIVER/i2cdriver.cpp
@@ -0,0 +1,35 @@
+#include "i2cdriver.h"
+
+QMutex i2cDriver::mutex;
+
+
+i2cDriver::i2cDriver(int deviceAddress, QObject *parent)
+    : QObject(parent), deviceAddress(deviceAddress), handle(-1)
+{
+
+}
+
+i2cDriver::~i2cDriver()
+{
+    if(handle >= 0){  //
+        i2cClose(handle);
+    }
+}
+
+bool i2cDriver::initialize()
+{
+    QMutexLocker locker(&mutex);  //
+
+    handle = i2cOpen(1, deviceAddress, 0);  //
+    if(handle < 0){
+        qDebug() << "Failed to open I2C device : " << deviceAddress;
+        return false;
+    }
+    qDebug() << "I2C device opened: " << QString::number(deviceAddress, 16);
+    return true;
+}
+
+QByteArray i2cDriver::readBytes(quint8 registerAddress, quint8 count)
+{
+    QByteArray data(count, 0);  //
+}
